92355fdac07629988893aeeb699a1de02d143ec3
Oleksandr Denysenko <oleksandr.denysenko@oracle.com>
2019-12-13 01:14:10 +0200

    poc

---
+++ js/keyboard.js
@@ -0,0 +1,199 @@
+(function() {
+  var Keyboard;
+
+  Keyboard = function(source) {
+    this.map = {
+      "esc": "besc",
+      "f1": "bf1",
+      "f2": "bf2",
+      "f3": "bf3",
+      "f4": "bf4",
+      "f5": "bf5",
+      "f6": "bf6",
+      "f7": "bf7",
+      "f8": "bf8",
+      "f9": "bf9",
+      "f10": "bf10",
+      "f11": "bf11",
+      "f12": "bf12",
+      "~": "btilda",
+      "-": "bminus",
+      "_": "bminus",
+      "+": "bplus",
+      "=": "bplus",
+      "[": "bsqbro",
+      "]": "bscbrc",
+      ";": "bsemicolon",
+      ":": "bsemicolon",
+      "'": "bapostrophe",
+      "\"": "bapostrophe",
+      "enter": "benter",
+      ",": "bcomma",
+      ".": "bpoint",
+      "\\": "bslash",
+      "|": "bslash",
+      "/": "bslash",
+      "?": "bquestion",
+      "shift": "bshift",
+      "shiftr": "bshiftr",
+      "ctrl": "bctrl",
+      "win": "bwin",
+      "alt": "balt",
+      "altr": "baltr",
+      "winr": "bwinr",
+      "menu": "bmenu",
+      "ctrlr": "bctrlr"
+    };
+    return this;
+  };
+
+  Keyboard.prototype.getHTML = function() {
+    return `
+          <div>
+              <div class="row">
+                  <div class="button btilda">\`<sub>~</sub></div>
+                  <div class="button b1">1<sub>!</sub></div>
+                  <div class="button b2">2<sub>@</sub></div>
+                  <div class="button b3">3<sub>#</sub></div>
+                  <div class="button b4">4<sub>$</sub></div>
+                  <div class="button b5">5<sub>%</sub></div>
+                  <div class="button b6">6<sub>^</sub></div>
+                  <div class="button b7">7<sub>&</sub></div>
+                  <div class="button b8">8<sub>*</sub></div>
+                  <div class="button b9">9<sub>(</sub></div>
+                  <div class="button b0">0<sub>)</sub></div>
+                  <div class="button bminus">-<sub>_</sub></div>
+                  <div class="button bplus">+<sub>=</sub></div>
+                  <div class="button jumper">back</div>
+              </div>
+              <div class="row">
+                  <div class="button jumper">tab</div>
+                  <div class="button bq">Q</div>
+                  <div class="button bw">W</div>
+                  <div class="button be">E</div>
+                  <div class="button br">R</div>
+                  <div class="button bt">T</div>
+                  <div class="button by">Y</div>
+                  <div class="button bu">U</div>
+                  <div class="button bi">I</div>
+                  <div class="button bo">O</div>
+                  <div class="button bp">P</div>
+                  <div class="button bsqbro">[</div>
+                  <div class="button bscbrc">]</div>
+                  <div class="button jumper">\</div>
+              </div>
+              <div class="row">
+                  <div class="button jumper">caps lock</div>
+                  <div class="button ba">A</div>
+                  <div class="button bs">S</div>
+                  <div class="button bd">D</div>
+                  <div class="button bf helper">F</div>
+                  <div class="button bg">G</div>
+                  <div class="button bh">H</div>
+                  <div class="button bj helper">J</div>
+                  <div class="button bk">K</div>
+                  <div class="button bl">L</div>
+                  <div class="button bsemicolon">;<sub>:</sub></div>
+                  <div class="button bapostrophe">'<sub>"</sub></div>
+                  <div class="button benter jumper">enter</div>
+              </div>
+              <div class="row">
+                  <div class="button bshift jumper">shift</div>
+                  <div class="button bz">Z</div>
+                  <div class="button bx">X</div>
+                  <div class="button bc">C</div>
+                  <div class="button bv">V</div>
+                  <div class="button bb">B</div>
+                  <div class="button bn">N</div>
+                  <div class="button bm">M</div>
+                  <div class="button bcomma">,<sub><</sub></div>
+                  <div class="button bpoint">.<sub>></sub></div>
+                  <div class="button bslash">/<sub>?</sub></div>
+                  <div class="button bshiftr jumper">shift</div>
+              </div>
+              <div class="row">
+                  <div class="button bctrl">ctl</div>
+                  <div class="button bfn">fn</div>
+                  <div class="button bwin">opt</div>
+                  <div class="button balt">alt</div>
+                  <div class="button bspace jumper">space</div>
+                  <div class="button baltr">alt</div>
+                  <div class="button bmenu">opt</div>
+                  <div class="button bctrlr">ctl</div>
+              </div>
+        </div>`;
+  }
+
+  Keyboard.prototype.getTypingHTML = function () {
+    return `<span id="before"></span>
+            <span id="line" class="typeNext"></span>
+            <span id="after"></span>`;
+  }
+
+  Keyboard.prototype.getButtonClass = function(alias) {
+    return this.map[alias] || "b" + alias;
+  };
+
+  Keyboard.prototype.pressButton = function(button, add) {
+    var className, lowerButton, upperButton;
+
+    add = add || false;
+
+    lowerButton = button.toLowerCase();
+    upperButton = button.toUpperCase();
+
+    const isCaseSensitive = lowerButton !== upperButton;
+
+    if (!add) {
+      document.querySelectorAll(".active").removeClass("active");
+    }
+
+    className = this.getButtonClass(lowerButton);
+    try {
+      document.querySelectorAll('.' + className).addClass("active");
+    } catch (ex) {
+      console.log('Class missed', '.' + className);
+    }
+
+
+    if (isCaseSensitive && button === upperButton) {
+      document.querySelectorAll(".bshift").addClass("active");
+    }
+  };
+
+  Element.prototype.addClass = function(c) {
+    this.className += " " + c + " ";
+    return this;
+  };
+
+  NodeList.prototype.addClass = function(c) {
+    var el, _i, _len;
+    for (_i = 0, _len = this.length; _i < _len; _i++) {
+      el = this[_i];
+      el.addClass(c);
+    }
+    return this;
+  };
+
+  Element.prototype.removeClass = function(c) {
+    var r;
+    r = new RegExp("\\s" + c + '\\s?', 'gi');
+    this.className = (" " + this.className).replace(r, '');
+    if (this.className[0] === ' ') {
+      this.className = this.className.substr(1);
+    }
+    return this;
+  };
+
+  NodeList.prototype.removeClass = function(c) {
+    var el, _i, _len;
+    for (_i = this.length - 1; _i >= 0; _i--) {
+      el = this[_i];
+      el.removeClass(c);
+    }
+    return this;
+  };
+
+  window.Keyboard = Keyboard;
+
+}).call(this);
---
+++ js/lesson-controller.js
@@ -0,0 +1,430 @@
+class LessonController {
+    constructor({ file, element }) {
+        this.element = element;
+        this.title = '';
+        this.lines = [];
+
+        const url = `https://raw.githubusercontent.com/abyr/typeggio-sources/master/qwerty/${file}`;
+
+        this.init({ url });
+    }
+
+    async init({ url }) {
+        for await (let line of makeTextFileLineIterator(url)) {
+            if (this.title) {
+                this.lines.push(line);
+            } else {
+                this.title = line;
+            }
+        }
+
+        const title = this.title;
+        const text = this.lines.join('\n');
+
+        this.lessonModel = new LessonModel({ title, text });
+        this.view = new LessonView(this.element, this.lessonModel);
+
+        this.start();
+    }
+
+    start() {
+        this.view.render();
+    }
+
+    destroy() {
+        this.view.destroy();
+    }
+}
+
+class LessonModel {
+    constructor({title, text}) {
+        this.title = title;
+        this.text = text;
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    getText() {
+        return this.text;
+    }
+}
+
+const NEXT_LINE_CODE = 'Enter';
+
+class LessonView {
+    constructor(element, lessonModel) {
+        this.element = element;
+        this.lesson = lessonModel;
+    }
+
+    render() {
+        const container = document.createElement("div");
+
+        const titleEl = document.createElement("h2");
+        const textEl = document.createElement("pre");
+        const hintEl = document.createElement("div");
+        const keyboardEl = document.createElement("div");
+
+        titleEl.innerHTML = this.lesson.getTitle();
+        textEl.innerHTML = this.lesson.getText();
+
+        this.keyboard = new Keyboard();
+
+        hintEl.innerHTML = this.keyboard.getTypingHTML();
+        keyboardEl.classList.add('keyboard');
+
+        keyboardEl.innerHTML = this.keyboard.getHTML();
+        container.append(titleEl, textEl, keyboardEl, hintEl);
+
+        this.element.appendChild(container);
+
+        this.textIterator = new TextIterator(this.lesson.getText());
+
+        var line = this.textIterator.currentLine();
+
+        this.typingHint = new TypingHint({
+            elDone: document.getElementById("before"),
+            elNow: document.getElementById("line"),
+            elNext: document.getElementById("after")
+        });
+
+        this.typingHint.render('', line[0], line.substring(1));
+
+        this.renderKeyboard();
+
+        document.onkeypress = this.onType.bind(this);
+    }
+
+    onType(e) {
+        var typingHint = this.typingHint;
+
+        e = e || window.event;
+
+        const pressedCode = (e.which ? e.which : e.keyCode);
+        const neededCode = this.textIterator.currentLineChar().charCodeAt();
+
+        if (pressedCode !== neededCode) {
+            return false;
+        }
+
+        const hasNextLine = this.textIterator.hasNextLine();
+        const hasNextLineChar = this.textIterator.hasNextLineChar();
+
+        if (hasNextLineChar) {
+            this.goToNextChar();
+
+        } else if (hasNextLine) {
+            this.goToNextLine();
+
+        } else {
+            this.finish();
+        }
+
+        this.renderKeyboard();
+    }
+
+    goToNextChar() {
+        this.typingHint.renderInc();
+        this.textIterator.next();
+    }
+
+    goToNextLine() {
+        this.textIterator.next();
+
+        const line = this.textIterator.currentLine();
+
+        this.typingHint.render('', line[0], line.substring(1));
+    }
+
+    renderKeyboard() {
+        const nextChar = this.textIterator.currentLineChar();
+
+        this.keyboard.pressButton(charToCode(nextChar));
+    }
+
+    finish() {
+        this.typingHint.render('Done!', '', '');
+        document.onkeypress = null;
+    }
+
+    destroy() {
+        while (this.element.firstChild) {
+            this.element.removeChild(this.element.firstChild);
+        }
+    }
+}
+
+class TypingHint {
+    constructor({ elDone, elNow, elNext }) {
+        this.elDone = new ElementWrapper(elDone);
+        this.elNow = new ElementWrapper(elNow);
+        this.elNext = new ElementWrapper(elNext);
+    }
+
+    isNextLine() {
+        return this.elNow.text() === NEXT_LINE_CODE;
+    }
+
+    isEOL() {
+        return this.elNext.len() === 0;
+    }
+
+    render(a, b, c) {
+        this.renderDone(a);
+        this.renderNow(b);
+        this.renderNext(c);
+    }
+
+    renderDone(x) {
+        this.elDone.text(x);
+    }
+
+    renderNow(x) {
+        this.elNow.text(x);
+    }
+
+    renderNext(x) {
+        this.elNext.text(x);
+    }
+
+    renderInc() {
+        this.renderDoneInc();
+        this.renderNowInc();
+        this.renderNextInc();
+    }
+
+    renderDoneInc() {
+        const c = this.elDone.text();
+
+        this.elDone.text(c + this.elNow.text().replace('&nbsp;', ' '));
+    }
+    renderNowInc() {
+        const c = this.elNext.text();
+        const ch = c[0] || '';
+
+        this.elNow.text(ch.replace(' ', '&nbsp;'));
+    }
+
+    renderNextInc() {
+        const c = this.elNext.text();
+
+        this.elNext.text(c.substring(1));
+    }
+}
+
+class ElementWrapper {
+    constructor(el) {
+        this.el = el;
+    }
+
+    text(x) {
+        if (typeof x === 'undefined') {
+            return this.getText();
+        } else {
+            return this.render(x);
+        }
+    }
+
+    getText() {
+        return this.el.innerHTML || '';
+    }
+
+    render(x) {
+        this.el.innerHTML = x;
+    }
+
+    len() {
+        return this.el.innerHTML.length;
+    }
+}
+
+const codesMap = {
+    "10": "enter",
+    "13": "enter",
+    "32": "space"
+};
+
+function charToCode(char) {
+    const initialCode = char.charCodeAt();
+    let code;
+
+    if (initialCode === 10) {
+        code = 13;
+    } else {
+        code = initialCode;
+    }
+
+    const specialCode = codesMap[code];
+
+    if (specialCode) {
+        return specialCode;
+    } else {
+        return char;
+    }
+}
+
+class TextIterator {
+    constructor(text) {
+        this.lines = text2list(text);
+
+        this.first();
+    }
+
+    first() {
+        this.lineIndex = 0;
+        this.charIndex = 0;
+
+        return this.currentLineChar();
+    }
+
+    next() {
+        if (this.hasNextLineChar()) {
+            return this.nextLineChar();
+
+        } else if (this.hasNextLine()) {
+            this.nextLine();
+
+            return this.currentLineChar();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @returns {String|Null}
+     */
+    nextLine() {
+        if (this.hasNextLine()) {
+            this.lineIndex++;
+            this.charIndex = 0;
+
+            return this.currentLine();
+        } else {
+
+            return null;
+        }
+
+    }
+
+    /**
+     * @returns {String|Null}
+     */
+    nextLineChar() {
+        if (this.hasNextLineChar()) {
+            this.charIndex++;
+
+            return this.currentLineChar();
+        } else {
+
+            return null;
+        }
+    }
+
+    /**
+     * @returns {String}
+     */
+    currentLineChar() {
+        var line = this.currentLine();
+
+        return line[this.charIndex];
+    }
+
+    /**
+     * @returns {String}
+     */
+    currentLine() {
+        return this.lines[this.lineIndex];
+    }
+
+    /**
+     * @returns {Boolean}
+     */
+    isDone() {
+        if (this.hasNextLine() || this.hasNextLineChar()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * @returns {Boolean}
+     */
+    hasNextLine() {
+        return !!this.lines[this.lineIndex + 1];
+    }
+
+    /**
+     * @returns {Boolean}
+     */
+    hasNextLineChar() {
+        return !!this.lines[this.lineIndex][this.charIndex + 1];
+    }
+
+    /**
+     *
+     * @param {Number} lineIndex
+     * @param {Number} charIndex
+     */
+    getByIndex(lineIndex, charIndex) {
+        const line = this.lines[lineIndex];
+
+        if (line) {
+            return line[charIndex] || null;
+        } else {
+            return null;
+        }
+    }
+}
+
+function text2list(text) {
+    return stripSpaces(text).match(/[^\r\n]+/g);
+}
+
+function stripSpaces(text) {
+    return text.replace(/^\s+|\s+$/gm, '');
+}
+
+async function* makeTextFileLineIterator(fileURL) {
+    const utf8Decoder = new TextDecoder('utf-8'),
+        response = await fetch(fileURL),
+        reader = response.body.getReader();
+
+    let {
+        value: chunk,
+        done: readerDone
+    } = await reader.read();
+
+    chunk = chunk ? utf8Decoder.decode(chunk) : '';
+
+    const re = /\n|\r|\r\n/gm;
+    let startIndex = 0;
+
+    for (; ;) {
+        let result = re.exec(chunk);
+
+        if (!result) {
+            if (readerDone) {
+                break;
+            }
+
+            let remainder = chunk.substr(startIndex);
+
+            ({ value: chunk, done: readerDone } = await reader.read());
+
+            chunk = remainder + (chunk ? utf8Decoder.decode(chunk) : '');
+            startIndex = re.lastIndex = 0;
+
+            continue;
+        }
+
+        yield chunk.substring(startIndex, result.index);
+        startIndex = re.lastIndex;
+    }
+
+    if (startIndex < chunk.length) {
+        yield chunk.substr(startIndex);
+    }
+}
