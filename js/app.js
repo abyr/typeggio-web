8b97580827a62d1a1551954376a6895c623549fe
Oleksandr Denysenko <aleksandr.denisenko.it@gmail.com>
2020-10-14 23:12:32 +0300

    separate lessons and stats controls

--- js/app.js
+++ js/app.js
@@ -3,46 +3,16 @@ import ResultsView from './results/results-view.js';
 import Lesson from './lesson/lesson.js';
 import Statist from "./statist.js";
 
-
-let lesson;
-
 const results = new Results();
 
+let lesson;
 let resultsView;
 
-function saveLessonResult(evnt) {
-    const { result, layout, lessonNumber } = evnt.detail;
+const screenController = {
+    lessonLayout: () => {
+        screenController.clearLayout();
 
-    results.save(`${layout}-${lessonNumber}`, result);
-}
-
-window.onload = () => {
-
-    var allResults = results.getAll();
-
-    resultsView = new ResultsView({
-        element: document.getElementById('results-view-container'),
-        allResults: allResults
-    });
-    resultsView.render();
-
-    document.getElementById('starter').addEventListener('click', e => {
-        if (lesson) {
-            lesson.element.removeEventListener('lesson:finished', saveLessonResult);
-            lesson.destroy();
-            lesson = null;
-        }
-
-        if (resultsView) {
-            resultsView.destroy();
-            resultsView = null;
-        }
-
-        e.target.blur();
-
-        const path = document.location.pathname;
-
-        const layout = path.substr(path.lastIndexOf('/') + 1, path.indexOf('.') -1 - path.lastIndexOf('/'));
+        const layout = document.querySelector('#lesson-select').getAttribute('data-layout');
 
         const statist = new Statist();
 
@@ -54,14 +24,70 @@ window.onload = () => {
         });
 
         lesson.element.addEventListener('lesson:finished', saveLessonResult);
+    },
+
+    landingLayout: () => {
+        resultsView = new ResultsView({
+            element: document.getElementById('results-view-container'),
+            allResults: results.getAll()
+        });
+        resultsView.render();
+    },
+
+    clearLayout: () => {
+        screenController.hideResultsControls();
+
+        if (lesson) {
+            lesson.element.removeEventListener('lesson:finished', e => {
+                saveLessonResult(e);
+
+                screenController.landingLayout();
+            });
+
+            lesson.destroy();
+            lesson = null;
+        }
+
+        if (resultsView) {
+            resultsView.destroy();
+            resultsView = null;
+        }
+    },
+
+    hideResultsControls: () => {
+        hideEl(document.querySelector('#results-controls'));
+    }
+};
+
+
+window.onload = () => {
+
+    screenController.landingLayout();
+
+    document.getElementById('starter').addEventListener('click', e => {
+        e.target.blur();
+        screenController.lessonLayout();
     });
 
     document.getElementById('exporter').addEventListener('click', e => {
         results.export();
     });
 
-
     document.getElementById('importer').addEventListener('change', e => {
         results.importFile(e);
     });
+}
+
+function saveLessonResult(evnt) {
+    const { result, layout, lessonNumber } = evnt.detail;
+
+    results.save(`${layout}-${lessonNumber}`, result);
+}
+
+function hideEl(el) {
+    el.style.display = 'none';
+}
+
+function showEl(el) {
+    el.style.display = '';
 }
