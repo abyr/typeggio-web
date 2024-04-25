29a4782564078a68c27f34969a14d60fa1924129
Oleksandr Denysenko <aleksandr.denisenko.it@gmail.com>
2024-04-25 07:44:07 +0000

    remove extra code

--- tests/libs/doc.js
+++ tests/libs/doc.js
@@ -24,8 +24,6 @@ class ClassList {
     remove(x) {
         const idx = this.classList.indexOf(x);
 
-        console.log('splice idx', idx, x, 'in', this.classList);
-
         if (idx > -1) {
             this.classList.splice(idx, 1);
         }
